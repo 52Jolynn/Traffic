457f77444806c217fecac30651946f3fd256a61e
52Jolynn <htd0324@gmail.com>
2012-12-10 10:00:20 +0800

    加入TEST

---
+++ traffic/src/test/java/com/laud/major/traffic/TrafficTest.java
@@ -0,0 +1,11 @@
+package com.laud.major.traffic;
+
+/**
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2012-12-10 上午9:59:43
+ * @copyright: www.armisi.com.cn
+ */
+public class TrafficTest {
+
+}
